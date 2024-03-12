8101222c80fc00b11720792989512172ae5af45c
krohitr2000 <krohitr2000@gmail.com>
2024-03-12 19:13:18 +0530

    first commit

---
+++ Mavericks Bank/server-side/src/app/forgot-password/forgot-password.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { SeriveService } from '../customer/signup/signupserive.service';
+import { Router } from '@angular/router';
+import { CustomersModel } from '../customer/CustomerModel';
+
+@Component({
+  selector: 'app-forgot-password',
+  templateUrl: './forgot-password.component.html',
+  styleUrls: ['./forgot-password.component.css']
+})
+export class ForgotPasswordComponent implements OnInit {
+  model: any = {}; 
+  constructor(private http: HttpClient, private service: SeriveService, private route: Router) {}
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    this.service.verify(this.model).subscribe(
+      (res: CustomersModel) => {
+        let customerId = res.customerId;
+        this.route.navigate(["/confirm-password", customerId]);
+      },
+      (error) => {
+        alert("Invalid Details");
+        window.location.reload();
+      }
+    );
+  }
+}
